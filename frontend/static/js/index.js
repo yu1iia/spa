7b14a2f67f8edef18b7cc1f8b1312030b3314651
Yuliia Burko <yuliia.burko@gmail.com>
2023-06-25 16:19:14 +0200

    added url parameters

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,6 +1,23 @@
 import Dashboard from './views/Dashboard.js';
 import Posts from './views/Posts.js';
+import PostView from './views/PostView.js';
 import Settings from './views/Settings.js';
+
+const pathToRegex = path =>
+  new RegExp('^' + path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)') + '$');
+
+const getParams = match => {
+  const values = match.result.slice(1);
+  const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(
+    result => result[1],
+  );
+
+  return Object.fromEntries(
+    keys.map((key, i) => {
+      return [key, values[i]];
+    }),
+  );
+};
 
 const navigateTo = url => {
   history.pushState(null, null, url);
@@ -8,9 +25,11 @@ const navigateTo = url => {
 };
 
 const router = async () => {
+  // posts/:id
   const routes = [
     { path: '/', view: Dashboard },
     { path: '/posts', view: Posts },
+    { path: '/posts/:id', view: PostView },
     { path: '/settings', view: Settings },
   ];
 
@@ -19,20 +38,22 @@ const router = async () => {
   const potentialMatches = routes.map(route => {
     return {
       route: route,
-      isMatch: location.pathname === route.path,
+      result: location.pathname.match(pathToRegex(route.path)),
     };
   });
 
-  let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+  let match = potentialMatches.find(
+    potentialMatch => potentialMatch.result !== null,
+  );
 
   if (!match) {
     match = {
       route: routes[0],
-      isMatch: true,
+      result: [location.pathname],
     };
   }
 
-  const view = new match.route.view();
+  const view = new match.route.view(getParams(match));
 
   document.querySelector('#app').innerHTML = await view.getHtml();
 };
--- frontend/static/js/views/PostView.js
+++ frontend/static/js/views/PostView.js
@@ -3,10 +3,11 @@ import AbstractView from './AbstractView.js';
 export default class extends AbstractView {
   constructor(params) {
     super(params);
-    this.setTitle('Posts');
+    this.setTitle('Viewing Posts');
   }
 
   async getHtml() {
+    console.log(this.params.id);
     return `
             <h1>Posts</h1>
          <p> You are viewing the posts!</p>
